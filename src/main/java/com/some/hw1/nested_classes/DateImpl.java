44156d95ba343ad1b3b5f98285321f1572b453a7
Artem Iurchenko <artem_iurchenko@epam.com>
2016-10-31 14:35:21 +0200

    renamed hw1 directory

--- src/main/java/com/some/hw1/nested_classes/DateImpl.java
+++ src/main/java/com/some/hw1/nested_classes/DateImpl.java
@@ -1,4 +1,4 @@
-package com.some.hw1.anonymus_classes;
+package com.some.hw1.nested_classes;
 
 /**
  * Created by User on 10/30/2016.
--- src/main/java/com/some/hw1/nested_classes/Runner.java
+++ src/main/java/com/some/hw1/nested_classes/Runner.java
@@ -1,4 +1,4 @@
-package com.some.hw1.anonymus_classes;
+package com.some.hw1.nested_classes;
 
 /**
  * Created by User on 10/30/2016.
@@ -7,7 +7,7 @@ public class Runner {
     public static void main(String[] args) {
         DateImpl date = new DateImpl(1,2,3);
 
-        date.getDayOfWeek(1);
+        date.getDayOfWeek();
         //System.out.println(date.getDayOfWeek(1));
         System.out.println(DayOfWeek.valueOf("MONDAY"));
         for(DayOfWeek day: DayOfWeek.values())
