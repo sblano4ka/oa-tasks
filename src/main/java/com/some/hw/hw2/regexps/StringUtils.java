c9a25ced555890945d54ed727e7a6e8ac2c667dd
Artem Iurchenko <artem_iurchenko@epam.com>
2016-11-07 19:37:36 +0200

    me - flipWord + palindrome done

--- src/main/java/com/some/hw/hw2/regexps/StringUtils.java
+++ src/main/java/com/some/hw/hw2/regexps/StringUtils.java
@@ -1,11 +1,41 @@
 package com.some.hw.hw2.regexps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Artem_Iurchenko on 07.11.2016.
  */
 public class StringUtils {
-    public void flipString(String string){}
-    public boolean isStringPalindrome(){return true;}
+    public static String flipString(String string){
+        String result = "";
+        List<Character> charsList = new ArrayList<>();
+        for(int i=string.length()-1;i>=0;i--){
+            Character c = string.toLowerCase().charAt(i);
+            charsList.add(c);
+        }
+        for (Character aCharsList : charsList) {
+            result = result + aCharsList;
+        }
+        return result;
+    }
+
+    public static boolean isStringPalindrome(String string){
+        boolean flag=false;
+        String result = "";
+        String inWork=string.replaceAll(" ","").toLowerCase();
+        List<Character> charsList = new ArrayList<>();
+        for(int i=inWork.length()-1;i>=0;i--){
+            Character c = inWork.charAt(i);
+            charsList.add(c);
+        }
+        for (Character aCharsList : charsList) {
+            result = result + aCharsList;
+        }
+        flag = result.equals(inWork);
+        return flag;}
+
+
     public void leftCutRightPad(String string){}
     public void flipWordsInString(String string){}
     public void flipWordsInSentence(String string){}
@@ -16,4 +46,11 @@ public class StringUtils {
         int []array = new int[0];
         return array;}
 
+    public static void main(String[] args) {
+        String toTest="!hcuo";
+
+
+        System.out.println( flipString(toTest));
+        System.out.println(isStringPalindrome("А роза упала на лапу Азора"));
+    }
 }
