c524f1e2948b268e5d75d7faaac28a767c9c8de1
Artem Iurchenko <artem_iurchenko@epam.com>
2016-11-07 19:17:29 +0200

    me - added hw2 files

---
+++ src/main/java/com/some/hw/hw2/regexps/MarkDownParser.java
@@ -0,0 +1,7 @@
+package com.some.hw.hw2.regexps;
+
+/**
+ * Created by Artem_Iurchenko on 07.11.2016.
+ */
+public class MarkDownParser {
+}
---
+++ src/main/java/com/some/hw/hw2/regexps/StringUtils.java
@@ -0,0 +1,19 @@
+package com.some.hw.hw2.regexps;
+
+/**
+ * Created by Artem_Iurchenko on 07.11.2016.
+ */
+public class StringUtils {
+    public void flipString(String string){}
+    public boolean isStringPalindrome(){return true;}
+    public void leftCutRightPad(String string){}
+    public void flipWordsInString(String string){}
+    public void flipWordsInSentence(String string){}
+    public boolean isABCcontains(String string){return true;}
+    public boolean isFormatMMDDYYYY(String string){return false;}
+    public boolean isAddress(String string){return true;}
+    public int[] findTelephoneNumbers(String string) {
+        int []array = new int[0];
+        return array;}
+
+}
